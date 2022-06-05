e6b1469fb427ee5096b91791c132cde3c6d54853
HoonDragonite <lsoonnoon@gmail.com>
2022-06-05 15:10:46 +0900

    Spring Security 설정 1. RoleType 정의 2. URL별 권한 설정 3. 인증, 인가 실패 시 Handler 구현 4. Password 암호화 Bean 생성

--- auth/src/main/java/com/kingpiggy/study/configuration/SecurityConfiguration.java
+++ auth/src/main/java/com/kingpiggy/study/configuration/SecurityConfiguration.java
@@ -1,4 +1,60 @@
 package com.kingpiggy.study.configuration;
 
-public class SecurityConfiguration {
+import com.kingpiggy.study.enumclass.RoleType;
+import com.kingpiggy.study.web.CustomAccessDeniedHandler;
+import com.kingpiggy.study.web.CustomAuthenticationEntryPoint;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+//    @Autowired
+//    private JwtAuthenticationProvider provider;
+
+    @Autowired
+    private CustomAuthenticationEntryPoint entryPoint;
+
+    @Autowired
+    private CustomAccessDeniedHandler accessDeniedHandler;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .formLogin().disable()
+                .logout().logoutUrl("/logout").permitAll()
+                .deleteCookies("accessToken")
+                .logoutSuccessUrl("/")
+                .and()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/admin/**")
+                .hasAnyRole(RoleType.ROLE_ADMIN.getKey())
+                .antMatchers("/**")
+                .permitAll()
+                .and()
+//                .addFilterBefore(new JwtAuthenticationFilter(authenticationManager(), provider),
+//                        UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(entryPoint)
+                .accessDeniedHandler(accessDeniedHandler);
+    }
+
+    @Bean
+    public PasswordEncoder PasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
